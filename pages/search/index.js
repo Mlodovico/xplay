7e27db9dcd95a03a2095e04ffd9c66ab8abd9550
mlodovico <murilo.lodovico@outlook.com>
2024-02-03 15:47:04 -0300

    feature: search component

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,8 +1,39 @@
 
+import { useEffect, useState } from "react";
+import Image from "next/image"
+
+import { Container, SearchInput, SearchInputContainer, Button } from "./styles";
+
+import SearchInpuIcon from "../../public/search_input_icon.svg"
+import CloseIcon from "../../public/close_icon.svg"
+
 export default function Search() {
+    const [search, setSearch] = useState('')
+
+    useEffect(() => {
+        handleSearch(search)
+    }, [search])
+
+    const handleSearch = (search) => {    
+        console.log(search)
+        //chamar rota filtrada
+    }
+
     return (
-        <div>
-            <h1>Search</h1>
-        </div>
+        <Container>
+            <SearchInputContainer>
+            <Image src={SearchInpuIcon} />
+            <SearchInput
+                type="text"
+                placeholder="Pesquisar"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                />
+            <Button onClick={() => setSearch('')}>
+            <Image src={CloseIcon} />
+
+            </Button>
+                </SearchInputContainer>
+       </Container>
     )
 }
---
+++ pages/search/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+`;
+
+export const SearchInput = styled.input`
+  height: 30px;
+  width: 85%;
+  background-color: transparent;
+  border: none;
+`;
+
+export const SearchInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #00000033;
+  border: none;
+  border-radius: 18px;
+  padding: 3px 10px;
+`;
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  border: none;
+`
