5e02195a700ca4811385332d0534f314961d831b
mlodovico <murilo.lodovico@outlook.com>
2024-02-03 15:08:24 -0300

    feature: selector component

--- components/SelectorBar/index.js
+++ components/SelectorBar/index.js
@@ -1,21 +1,36 @@
+import { useState } from "react";
 
-import { Container, Button } from './styles'
+import { Container, Button } from "./styles";
 
 export const SelectorBar = () => {
-    return (
-        <Container>
-            <Button>
-                Musicas
-            </Button>
-            <Button>
-                Albuns
-            </Button>
-            <Button>
-                Podcasts
-            </Button>
-            <Button>
-                Artistas
-            </Button>
-        </Container>
-    )
-}
+  const [typeSelector, setTypeSelector] = useState("music");
+
+  return (
+    <Container>
+      <Button
+        selected={typeSelector === "music"}
+        onClick={() => setTypeSelector("music")}
+      >
+        Musicas
+      </Button>
+      <Button
+        selected={typeSelector === "albums"}
+        onClick={() => setTypeSelector("albums")}
+      >
+        Albuns
+      </Button>
+      <Button
+        selected={typeSelector === "podcasts"}
+        onClick={() => setTypeSelector("podcasts")}
+      >
+        Podcasts
+      </Button>
+      <Button
+        selected={typeSelector === "artists"}
+        onClick={() => setTypeSelector("artists")}
+      >
+        Artistas
+      </Button>
+    </Container>
+  );
+};
--- components/SelectorBar/styles.js
+++ components/SelectorBar/styles.js
@@ -1,20 +1,27 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    height: 25px;
-    align-items: center;
-    justify-content: space-between;
-    width: 80%;
-    border-radius: 18px;
-    background-color: #00000033;
-`
+  display: flex;
+  height: 25px;
+  align-items: center;
+  justify-content: space-between;
+  width: 80%;
+  border-radius: 18px;
+  background-color: #00000033;
+  max-width: fit-content;
+`;
 
 export const Button = styled.button`
-    height: 100%;
-    color: #fff;
+  height: 100%;
+  color: #fff;
+  background-color: transparent;
+  font-size: 14px;
+  border: none;
+  border-radius: 18px;
+
+  ${(props) =>
+    props.selected &&
+    `
     background-color: #00000099;
-    font-size: 14px;
-    border: none;
-    border-radius: 18px;
-`
+    `}
+`;
